19167e89f32ebe092ea5d18f72c5e83db0065c99
Melokuhle <melokuhlemaphisa99@gmail.com>
2026-02-05 11:29:40 +0200

    feat: coded the Home page for user experience

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
+import ChatScreen from "../src/components/ChatScreen";
+import "./App.css";
+import HeroSlider from "./components/HeroSlider";
 
-
-import './App.css'
-
-function App() {
- 
-
+export default function App() {
   return (
-    <>
-      
-    </>
-  )
-}
+    <div className="app">
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="nav-left">mLab</div>
+        <div className="nav-right">
+          <a href="/" className="nav-link">
+            Home
+          </a>
+          <a href="/who-we-are" className="nav-link">
+            Who We Are
+          </a>
+          <a href="/what-we-do" className="nav-link">
+            What We Do
+          </a>
+          <a href="/partners" className="nav-link">
+            Partners
+          </a>
+          <a href="/news" className="nav-link">
+            News
+          </a>
+          <a href="/resources" className="nav-link">
+            Resources
+          </a>
+          <a href="/contact" className="nav-link">
+            Contact Us
+          </a>
+          <a href="/admin" className="nav-link">
+            Admin
+          </a>
+        </div>
+      </nav>
 
-export default App
+      <HeroSlider />
+      {/* Chatbot */}
+      <ChatScreen />
+    </div>
+  );
+}
