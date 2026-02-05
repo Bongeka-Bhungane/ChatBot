7ad97246df16f02cc656c0ab4eddd3264f23151b
ntorh <ngcobontombenhle434@gmail.com>
2026-02-05 11:38:33 +0200

    created stat component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,16 @@ import { useState } from 'react'
 
 
 import './App.css'
-import StatCard from './components/starCard';
 
 function App() {
  
 
   return (
     <>
-      <StatCard name="Total Users" value={128} />
-      <StatCard name="Active Sessions" value={34} />
-      <StatCard name="Revenue" value="$4,200" />
+    
+      
     </>
-  );
+  )
 }
 
 export default App
